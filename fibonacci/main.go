5ccd9648273576d351d741acdd1ee39ccd2be45d
André Gustavo de Andrade <andre.andrade@gympass.com>
2024-10-12 13:27:18 -0300

    chore: fibonacci sequence using recursive and interactive approach

---
+++ fibonacci/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+var ErrNegativeInput = errors.New("fibonacci is not defined for negative numbers")
+
+func main() {
+
+	fmt.Println(findFibonacciInteractive(-1))
+	fmt.Println(findFibonacciInteractive(0))
+	fmt.Println(findFibonacciInteractive(1))
+	fmt.Println(findFibonacciInteractive(10))
+	fmt.Println()
+	fmt.Println(findFibonacciRecursive(-1))
+	fmt.Println(findFibonacciRecursive(0))
+	fmt.Println(findFibonacciRecursive(1))
+	fmt.Println(findFibonacciRecursive(10))
+}
+
+func findFibonacciInteractive(value int) (int, error) {
+	switch {
+	case value < 0:
+		return -1, ErrNegativeInput
+	case value < 2:
+		return value, nil
+	}
+
+	prev, current := 0, 1
+	for i := 2; i <= value; i++ {
+		prev, current = current, prev+current
+	}
+
+	return current, nil
+}
+
+func findFibonacciRecursive(value int) (int, error) {
+	switch {
+	case value < 0:
+		return -1, ErrNegativeInput
+	case value < 2:
+		return value, nil
+	default:
+		v1, _ := findFibonacciRecursive(value - 1)
+		v2, _ := findFibonacciRecursive(value - 2)
+		return v1 + v2, nil
+	}
+}
