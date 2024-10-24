5d709919c141cc7465469ddd8006d68246097976
André Gustavo de Andrade <andre.andrade@gympass.com>
2024-10-24 08:47:51 -0300

    chore: minimum cost climbing stairs

---
+++ min_cost_climbing_stair/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(minCostClimbingStairs([]int{10, 15, 20}))
+	fmt.Println(minCostClimbingStairs([]int{1, 100, 1, 1, 1, 100, 1, 1, 100, 1}))
+}
+
+// minCostClimbingStairs time complexity O(n) space complexity O(1)
+func minCostClimbingStairs(cost []int) int {
+	if len(cost) == 1 {
+		return cost[0]
+	}
+
+	prev, curr := 0, 0
+
+	for i := 2; i < len(dp); i++ {
+		oneStep := curr + cost[i-1]
+		towSteps := prev + cost[i-2]
+		prev, curr = curr, min(oneStep, towSteps)
+	}
+
+	return curr
+}
