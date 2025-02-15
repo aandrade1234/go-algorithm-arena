7eda7eaecf529731b43894d48b1d28ba50d902ec
André Gustavo de Andrade <andre.andrade@gympass.com>
2025-02-15 14:29:46 -0300

    feat: implement merge sorted array solutions and add README
    Added two implementations for merging sorted arrays (`merge1` and `merge2`) with respective time and space complexities in `main.go`. Included examples and problem description in `README.md` for clarity. Updated `.golangci.yml` configuration to comment out `forbidigo`.

---
+++ merge_sorted_array/main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	nums1 := []int{1, 2, 3, 0, 0, 0}
+	nums2 := []int{2, 5, 6}
+	merge1(nums1, 3, nums2, 3)
+	fmt.Println(nums1)
+
+	nums1 = []int{1}
+	nums2 = []int{}
+	merge1(nums1, 1, nums2, 0)
+	fmt.Println(nums1)
+
+	nums1 = []int{0}
+	nums2 = []int{1}
+	merge1(nums1, 0, nums2, 1)
+	fmt.Println(nums1)
+
+	fmt.Println()
+
+	nums1 = []int{1, 2, 3, 0, 0, 0}
+	nums2 = []int{2, 5, 6}
+	merge1(nums1, 3, nums2, 3)
+	fmt.Println(nums1)
+
+	nums1 = []int{1}
+	nums2 = []int{}
+	merge2(nums1, 1, nums2, 0)
+	fmt.Println(nums1)
+
+	nums1 = []int{0}
+	nums2 = []int{1}
+	merge2(nums1, 0, nums2, 1)
+	fmt.Println(nums1)
+}
+
+// Time complexity O(m+n), space complexity O(1).
+func merge2(nums1 []int, m int, nums2 []int, n int) {
+	i, j, k := m-1, n-1, m+n-1
+
+	for i >= 0 && j >= 0 {
+		if nums1[i] > nums2[j] {
+			nums1[k] = nums1[i]
+			i--
+		} else {
+			nums1[k] = nums2[j]
+			j--
+		}
+		k--
+	}
+
+	for j >= 0 {
+		nums1[k] = nums2[j]
+		j--
+		k--
+	}
+}
+
+// Time complexity O(m+n), space complexity O(m+n).
+func merge1(nums1 []int, m int, nums2 []int, n int) {
+	result := make([]int, 0, m+n)
+
+	i, j := 0, 0
+	for {
+		if i < m && j < n {
+			if nums1[i] <= nums2[j] {
+				result = append(result, nums1[i])
+				i++
+			} else {
+				result = append(result, nums2[j])
+				j++
+			}
+		} else if i < m {
+			result = append(result, nums1[i])
+			i++
+		} else if j < n {
+			result = append(result, nums2[j])
+			j++
+		} else {
+			break
+		}
+	}
+
+	copy(nums1, result)
+}
