c378351b83ed39cc65a9c4bc7833308881876853
André Gustavo de Andrade <andre.andrade@gympass.com>
2025-02-16 13:02:23 -0300

    feat: add solution for "Is Subsequence" problem
    Add `isSubsequence` function to determine if one string is a subsequence of another. Includes main program with examples and a README for problem description, constraints, and examples.

---
+++ is_subsequence/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(isSubsequence("abc", "ahbgdc"))
+	fmt.Println(isSubsequence("axc", "ahbgdc"))
+	fmt.Println(isSubsequence("", "ahbgdc"))
+	fmt.Println(isSubsequence("aza", "abzba"))
+	fmt.Println(isSubsequence("abbc", "ahbdc"))
+
+}
+
+// Time complexity O(n), Space complexity O(1).
+func isSubsequence(s string, t string) bool {
+	pLeft, bLeft := 0, len(s)
+	pRight, bRight := 0, len(t)
+
+	for pLeft < bLeft && pRight < bRight {
+		if s[pLeft] == t[pRight] {
+			pLeft++
+		}
+
+		pRight++
+	}
+
+	return pLeft == bLeft
+}
